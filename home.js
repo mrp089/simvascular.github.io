e58158f5200980d768a0524149f47a31b28facf8
Carole <carole.darve@gmail.com>
2023-06-06 16:06:56 -0700

    header for bookmarks, attempting js

---
+++ home.js
@@ -0,0 +1,25 @@
+// function checkWidth();
+
+// function checkWidth() {
+//     //at 767px, screen is considered "small"
+//     if (screen.width >= 767 && (document.documentElement.clientWidth >= 767)) {
+//       if (smallScreen) {
+//         //updates smallScreen
+//         smallScreen = false;
+//         //updates counters
+//         updateMenuBar();
+//       }
+//     }
+//     else {
+//       if (!smallScreen) {
+//         //updates smallScreen
+//         smallScreen = true;
+//         //updates counters
+//         updateMenuBar();
+//       }
+//     }
+//   }
+  
+// function updateMenuBar() {
+
+// }
