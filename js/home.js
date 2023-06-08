8bf0c4f2588a821d9d93c05d4d996cbe1622c9ca
Carole <carole.darve@gmail.com>
2023-06-08 11:44:27 -0700

    added pictures fixed scroll

--- js/home.js
+++ js/home.js
@@ -17,12 +17,12 @@ function checkWidth()
 $(window).ready(checkWidth);
 $(window).resize(checkWidth);
 
-function clickLink(hrefTag)
-{
-    var a = document.createElement("a");
-    a.href = hrefTag;
-    a.click();
-}
+// function clickLink(hrefTag)
+// {
+//     var a = document.createElement("a");
+//     a.href = hrefTag;
+//     a.click();
+// }
 function toggleMenu(closeMenu = false)
 {
     var element = document.getElementById("contentOfSkip");
@@ -38,8 +38,8 @@ function toggleMenu(closeMenu = false)
         element.classList.remove("visible");
 
         if (smallScreen) {
-            $('.html').css({"overflow": "hidden"})
-            $('.body').css({"overflow": "hidden"})
+            $('.html').css({"overflow": "auto"})
+            $('.body').css({"overflow": "auto"})
         }
 
         navigationSection.classList.remove("menuOn");
@@ -52,8 +52,8 @@ function toggleMenu(closeMenu = false)
         element.classList.add("visible");
     
         if (smallScreen) {
-            $('.html').css({"overflow": "auto"})
-            $('.body').css({"overflow": "auto"})
+            $('.html').css({"overflow": "hidden"})
+            $('.body').css({"overflow": "hidden"})
         }
 
         navigationSection.classList.add("menuOn");
@@ -62,51 +62,67 @@ function toggleMenu(closeMenu = false)
     }
 }
 
+function scrollToTop() {
+
+    window.scrollTo({top: 0, behavior: 'smooth'});
+    
+}
+
+function smoothScrollingToTag(id)
+{
+    var element = document.getElementById(id);
+
+    console.log(element.getBoundingClientRect())
+    var position = element.getBoundingClientRect().top;
+    window.scrollTo({top: (position + scrollY - 65), behavior: 'smooth'});
+}
+
 $("#iconForSkip").click(function() {
     toggleMenu();
 });
 
 $("#skipToHome").click(function() {
-    clickLink("#home");
+    scrollToTop();
     toggleMenu(true);
 });
 
 $("#skipToGallery").click(function() {
-    clickLink("#gallery");
+    smoothScrollingToTag("gallery");
     toggleMenu(true);
 });
 
 $("#skipToDownload").click(function() {
-    clickLink("#download");
+    smoothScrollingToTag("download");
     toggleMenu(true);
 });
 
 $("#skipToDocumentation").click(function() {
-    clickLink("#documentation");
+    smoothScrollingToTag("documentation");
     toggleMenu(true);
 });
 
 $("#skipToOnline_Tutorials").click(function() {
-    clickLink("#tutor");
+    smoothScrollingToTag("tutor");
     toggleMenu(true);
 });
 
 $("#skipToRepository").click(function() {
-    clickLink("#repository");
+    smoothScrollingToTag("repository");
     toggleMenu(true);
 });
 
 $("#skipToWorkshops").click(function() {
-    clickLink("#workshops");
+    smoothScrollingToTag("workshops");
     toggleMenu(true);
 });
 
 $("#skipToTeam").click(function() {
-    clickLink("#team");
+    smoothScrollingToTag("team");
     toggleMenu(true);
 });
 
 $("#skipToContact").click(function() {
-    clickLink("#contact");
+    smoothScrollingToTag("contact");
     toggleMenu(true);
 });
+
