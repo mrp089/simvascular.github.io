6e3cf0ff8d21f313afdd89030d7710f5570e7f8c
Carole <carole.darve@gmail.com>
2023-06-28 16:03:16 -0700

    update documentation

--- documentation/compile/_additional_notes.md
+++ documentation/compile/_additional_notes.md
@@ -1,15 +1,14 @@
-##Additional Notes
+## Additional Notes
 
 ### Linux: Multiple Compilers
 
-
 Some versions of the needed software do not compile correctly using certain
-versions of gcc/g++.  We use gcc-4.6 to compile many of the external libraries.
+versions of gcc/g++. We use gcc-4.6 to compile many of the external libraries.
 It is easy to obtain gcc-4.6 without overwriting your current version using
-update-alternatives.  Below is an example of how to install both gcc-4.6 and
-gcc-4.8 on a ubuntu system.
+update-alternatives. Below is an example of how to install both gcc-4.6 and
+gcc-4.8 on a Ubuntu system.
 
-````bash
+```bash
 apt-get install c++-4.6 c++-4.8
 
 update-alternatives --remove-all gcc
@@ -29,7 +28,7 @@ update-alternatives --set c++ /usr/bin/g++
 
 update-alternatives --config gcc
 update-alternatives --config g++
-````
+```
 
 <br>
 <br>
--- js/documentation.js
+++ js/documentation.js
@@ -1,23 +1,152 @@
-$("#skipToTop").click(function() {
-  scrollToTop();
-  console.log("entersS")
-});
+getMarkdown("documentation/quickguide/_intro.md");
+getMarkdown("documentation/quickguide/_datamanager.md");
 
-$("#skipToInstitutions").click(function() {
-  smoothScrollingToTag("institutions");
-});
 
-function scrollToTop() {
-
-  window.scrollTo({top: 0, behavior: 'smooth'});
-  
-}
-
-function smoothScrollingToTag(id)
+function getMarkdown(mdFileURL)
 {
-    var element = document.getElementById(id);
+  $.ajax({
+    type: "GET",
+    url: mdFileURL,
+    dataType: "text",
+    async: false,
+    success: function(markdown) {
+      mdtext = markdown;
+    }
+  });
 
-    console.log(element.getBoundingClientRect())
-    var position = element.getBoundingClientRect().top;
-    window.scrollTo({top: (position + scrollY - 115), behavior: 'smooth'});
+  stringToHtml(mdtext)
 }
+
+function stringToHtml(mdtext) {
+  var htmlAsString = mdtext;
+  
+  htmlAsString = replaceWithHeader("###", htmlAsString);
+  htmlAsString = replaceWithHeader("##", htmlAsString);
+  htmlAsString = replaceWithHeader("#", htmlAsString);
+
+  htmlAsString = replaceInlineStyle("**", "<b>", "</b>", htmlAsString);
+  htmlAsString = replaceInlineStyle("*", "<i>", "</i>", htmlAsString);
+  htmlAsString = replaceInlineStyle("`", "<code>", "</code>", htmlAsString);
+
+  // htmlAsString = replaceInlineStyle(whatIsThere, whatToReplaceWith, htmlAsString);
+
+
+  
+  htmlAsString = removeNewLines(htmlAsString);
+  
+  var htmlAsNode = document.getElementById("intro");
+  htmlAsNode.insertAdjacentHTML("afterend", htmlAsString);
+}
+
+function replaceWithHeader(hashtags, string){
+  var headerElementStart = "<" + "h" + hashtags.length + ">";
+  var headerElementEnd = "</" + "h" + hashtags.length + ">";
+  while(string.indexOf(hashtags) != -1)
+  {
+    var index = string.indexOf("\n", string.indexOf(hashtags)) +  (4 - hashtags.length);
+    string = string.replace(hashtags, headerElementStart);
+    string = insert(string, index, headerElementEnd)
+  }
+
+  return string;
+}
+
+function replaceInlineStyle(whatIsThere, replaceWithFirst, replaceWithSecond, string)
+{
+  var isFirst = false;
+  while (string.indexOf(whatIsThere) != -1)
+  {
+    isFirst = !isFirst;
+    if(isFirst)
+    {
+      string = string.replace(whatIsThere, replaceWithFirst);
+    }
+    else
+    {
+      string = string.replace(whatIsThere, replaceWithSecond);
+    }
+  }
+
+  return string;
+}
+
+function insert(str, index, value) {
+  return str.substr(0, index) + value + str.substr(index);
+}
+
+function removeNewLines(string){
+  var isFirst = false;
+  while (string.indexOf("\n\n") != -1)
+  {
+    isFirst = !isFirst;
+    if(isFirst)
+    {
+      string = string.replace("\n\n", "<p>")
+    }
+    else
+    {
+      string = string.replace("\n\n", "</p>")
+    }
+  }
+
+  return string;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $("#skipToTop").click(function() {
+//   scrollToTop();
+//   console.log("entersS")
+// });
+
+// $("#skipToInstitutions").click(function() {
+//   smoothScrollingToTag("institutions");
+// });
+
+// function scrollToTop() {
+
+//   window.scrollTo({top: 0, behavior: 'smooth'});
+  
+// }
+
+// function smoothScrollingToTag(id)
+// {
+//     var element = document.getElementById(id);
+
+//     console.log(element.getBoundingClientRect())
+//     var position = element.getBoundingClientRect().top;
+//     window.scrollTo({top: (position + scrollY - 115), behavior: 'smooth'});
+// }
