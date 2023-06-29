08ebede72455784ff20b1d5f21fb4940ca755755
Carole <carole.darve@gmail.com>
2023-06-29 12:22:30 -0700

    nav bar, working markdown translation and styles

--- documentation/quickguide/_intro_tools.md
+++ documentation/quickguide/_intro_tools.md
@@ -1,6 +1,6 @@
 <h2 id="intro_tools"> Tools </h2>
 
-The SimVascular application provides a set of core <i>Tools</i> (aka plugins) implementing methods for each step in the 
+The SimVascular application provides a set of core <i>Tools</i> (aka plugins) implementing methods for each step in the
 SimVascular image-based modeling pipeline. The follow lists each core <i>Tool</i> type and primary function
 
 <ol>
@@ -11,25 +11,25 @@ SimVascular image-based modeling pipeline. The follow lists each core <i>Tool</i
   <li> <b>Simulations</b> - Performs a CFD simulation 
   <li> <b>svFSI</b> - Performs a coupled fluid-solid interaction simulation 
   <li> <b>ROMSimulations</b> - Performs a reduced-order model (ROM) simulation 
-</ol> 
+</ol>
 
 One or more instances of these <i>Tool</i> types are created to implement an image-based modeling pipeline for a particular
 application. An instance of a <i>Tool</i> is called a <i>Data Node</i>.
 
-<i>Tools</i> typically use data from other tools to perform their functions. For example, the <i>Segmentations Tool</i> uses 
-paths created by the <i>Paths Tool</i> to position and orient 2D slices in the 3D image volume. The data used by a 
+<i>Tools</i> typically use data from other tools to perform their functions. For example, the <i>Segmentations Tool</i> uses
+paths created by the <i>Paths Tool</i> to position and orient 2D slices in the 3D image volume. The data used by a
 <i>Tool</i> is specified by the user.
 
-The geometry created by each <i>Tool</i> is typically stored in <a href="https://vtk.org"> Visualization Toolkit (VTK) </a> 
+The geometry created by each <i>Tool</i> is typically stored in <a href="https://vtk.org"> Visualization Toolkit (VTK) </a>
 VTP and VTU <a href="https://vtk.org/wp-content/uploads/2015/04/file-formats.pdf"> VTK File Formats </a>.
 
-Each <i>Tool</i> has a panel containing GUI controls (i.e. widgets) used to specify the parameters that control tool behavior. 
+Each <i>Tool</i> has a panel containing GUI controls (i.e. widgets) used to specify the parameters that control tool behavior.
 A <i>Tool</i> panel is displayed using the <a href="#data_manager"> Data Manager </a>.
 
 <br>
 <h3 id="intro_tools_instances"> Creating Tool Instances</h3>
 An instance of a particular <i>Tool</i> type is created using options selected from <a href="#data_manager_menus">Data Manager Menus</a>. 
-Some <i>Tool</i> instances can be created in two ways based on the source of the data used by the <i>Tool</i> 
+Some <i>Tool</i> instances can be created in two ways based on the source of the data used by the <i>Tool</i>
 
 <ol>
   <li> <b>Internal</b> - Data used by the <i>Tool</i> is obtained from another <i>Tool</i>.<br><br> 
@@ -37,9 +37,6 @@ Some <i>Tool</i> instances can be created in two ways based on the source of the
 
   <li> <b>External</b> - Data used by the <i>Tool</i> is read in (imported) from a file.  The instances of <i>Paths</i>, 
                          <i>Segmentations</i> and <i>Models</i> <i>Tools</i> can be created by reading data from a file. <br><br>
-                         Example: <i>Models Tool</i> geometry is read in from a VTK VTP file.</li> 
+                         Example: <i>Models Tool</i> geometry is read in from a VTK VTP file.</li>
 
 </ol>
-
-
-
--- js/documentation.js
+++ js/documentation.js
@@ -1,152 +1,37 @@
-getMarkdown("documentation/quickguide/_intro.md");
-getMarkdown("documentation/quickguide/_datamanager.md");
+function scrollToTop() {
 
+    window.scrollTo({top: 0, behavior: 'smooth'});
+    
+}
 
-function getMarkdown(mdFileURL)
+function smoothScrollingToTag(id)
 {
-  $.ajax({
-    type: "GET",
-    url: mdFileURL,
-    dataType: "text",
-    async: false,
-    success: function(markdown) {
-      mdtext = markdown;
-    }
-  });
+    var element = document.getElementById(id);
 
-  stringToHtml(mdtext)
+    var position = element.getBoundingClientRect().top;
+    window.scrollTo({top: (position + scrollY - 90), behavior: 'smooth'});
 }
 
-function stringToHtml(mdtext) {
-  var htmlAsString = mdtext;
-  
-  htmlAsString = replaceWithHeader("###", htmlAsString);
-  htmlAsString = replaceWithHeader("##", htmlAsString);
-  htmlAsString = replaceWithHeader("#", htmlAsString);
+$("#skipToTop").click(function() {
+    scrollToTop();
+});
 
-  htmlAsString = replaceInlineStyle("**", "<b>", "</b>", htmlAsString);
-  htmlAsString = replaceInlineStyle("*", "<i>", "</i>", htmlAsString);
-  htmlAsString = replaceInlineStyle("`", "<code>", "</code>", htmlAsString);
+addHooks();
 
-  // htmlAsString = replaceInlineStyle(whatIsThere, whatToReplaceWith, htmlAsString);
-
-
-  
-  htmlAsString = removeNewLines(htmlAsString);
-  
-  var htmlAsNode = document.getElementById("intro");
-  htmlAsNode.insertAdjacentHTML("afterend", htmlAsString);
-}
-
-function replaceWithHeader(hashtags, string){
-  var headerElementStart = "<" + "h" + hashtags.length + ">";
-  var headerElementEnd = "</" + "h" + hashtags.length + ">";
-  while(string.indexOf(hashtags) != -1)
-  {
-    var index = string.indexOf("\n", string.indexOf(hashtags)) +  (4 - hashtags.length);
-    string = string.replace(hashtags, headerElementStart);
-    string = insert(string, index, headerElementEnd)
-  }
-
-  return string;
-}
-
-function replaceInlineStyle(whatIsThere, replaceWithFirst, replaceWithSecond, string)
+function addHooks()
 {
-  var isFirst = false;
-  while (string.indexOf(whatIsThere) != -1)
-  {
-    isFirst = !isFirst;
-    if(isFirst)
-    {
-      string = string.replace(whatIsThere, replaceWithFirst);
-    }
-    else
-    {
-      string = string.replace(whatIsThere, replaceWithSecond);
-    }
-  }
+    var skipToElements = document.getElementsByClassName("skipTo");
 
-  return string;
+    for(var i = 0; i < skipToElements.length; i++)
+    {
+        var id = skipToElements[i].id;
+        console.log(id);
+
+        $("#" + id).click(function() {
+            var id = $(this).attr("id").substring("skipTo".length)
+            smoothScrollingToTag(id);
+            
+        });
+        
+    }
 }
-
-function insert(str, index, value) {
-  return str.substr(0, index) + value + str.substr(index);
-}
-
-function removeNewLines(string){
-  var isFirst = false;
-  while (string.indexOf("\n\n") != -1)
-  {
-    isFirst = !isFirst;
-    if(isFirst)
-    {
-      string = string.replace("\n\n", "<p>")
-    }
-    else
-    {
-      string = string.replace("\n\n", "</p>")
-    }
-  }
-
-  return string;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// $("#skipToTop").click(function() {
-//   scrollToTop();
-//   console.log("entersS")
-// });
-
-// $("#skipToInstitutions").click(function() {
-//   smoothScrollingToTag("institutions");
-// });
-
-// function scrollToTop() {
-
-//   window.scrollTo({top: 0, behavior: 'smooth'});
-  
-// }
-
-// function smoothScrollingToTag(id)
-// {
-//     var element = document.getElementById(id);
-
-//     console.log(element.getBoundingClientRect())
-//     var position = element.getBoundingClientRect().top;
-//     window.scrollTo({top: (position + scrollY - 115), behavior: 'smooth'});
-// }
